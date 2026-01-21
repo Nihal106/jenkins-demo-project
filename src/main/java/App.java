67354ddd6bfdd1d92aea52ed1f02deb05d713727
Muhammed Nihal P K <muhammednihalpk2003@gmail.com>
2026-01-21 20:11:18 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins Maven Build!");
+        System.out.println("Hello from Jenkins Demo!");
+        while (true) {
+            try {
+                Thread.sleep(10000);
+            } catch (Exception e) {}
+        }
     }
 }
